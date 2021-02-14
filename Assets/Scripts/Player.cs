1e142a2e4c255453fffa72cdb03add8b2bc53663
YagetaTatatsuki <dragontree105@gmail.com>
2021-02-14 14:24:47 +0900

    プレイヤー操作更新

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float rechargeTime = 1f;     //連射間隔
 
-    Vector3 cameraDir = Vector3.zero;
-    Vector3 playerDir = Vector3.zero;
+    bool isFirePer = true;
+
+   
+   
+
+
+    public Vector3 cameraDir = Vector3.zero;
+    public Vector3 playerDir = Vector3.zero;
+    public Vector2 angle = Vector2.zero;
+
+    
+    public GameObject Bullet;       // bullet prefab
+
+
+    public Transform Muzzle;        // 弾丸発射点
+
+
+    //ジャンプに使用するもの
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    public float jumpPower = 15f;
+    public float walkSpeed = 6f;
+    public float fallSpeed = 0.5f;
+    public float runSpeed = 10f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -19,58 +45,157 @@ public class Player : MonoBehaviour
     {
         //移動//
 
-        //左に移動
-        if (Input.GetKey(KeyCode.A))
+       
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.y = 0.0f;
+        moveDirection.z = Input.GetAxis("Vertical");
+
+
+        if (Input.GetKey(KeyCode.LeftControl))
         {
-            this.transform.Translate(-0.1f, 0.0f, 0.0f);
+            moveDirection *= runSpeed;
         }
-        // 右に移動
-        if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Translate(0.1f, 0.0f, 0.0f);
-        }
-        // 前に移動
-        if (Input.GetKey(KeyCode.W))
-        {
-            this.transform.Translate(0.0f, 0.0f, -0.1f);
-        }
-        // 後ろに移動
-        if (Input.GetKey(KeyCode.S))
-        {
-            this.transform.Translate(0.0f, 0.0f, 0.1f);
-        }
-        //上に移動
+            moveDirection *= walkSpeed;
+        
+
         if (Input.GetKey(KeyCode.Space))
         {
-            this.transform.Translate(0.0f, 0.1f, 0.0f);
+            moveDirection.y += jumpPower;
         }
-        //下に移動
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            this.transform.Translate(0.0f, -0.1f, 0.0f);
-        }
+
+        moveDirection.y += Physics.gravity.y*fallSpeed; //重力計算
+        controller.Move(moveDirection * Time.deltaTime); //Playerを動かす処理
+
+        Debug.Log(walkSpeed);
 
 
 
 
 
         //向き//
-        
-        Vector2 angle = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        Debug.Log(angle);
+
+        Vector2 angle = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         cameraDir += new Vector3(-angle.y, angle.x, 0);
         Camera.main.transform.rotation=Quaternion.Euler(cameraDir);
 
         playerDir += new Vector3(-angle.y, angle.x, 0);
         this.transform.rotation = Quaternion.Euler(playerDir);
-        
-        //上を向く
-        /*if (Input.mousePosition.y)
+
+
+
+        //射撃//
+
+        if (Input.GetMouseButton(0)&& isFirePer)
         {
-            this.transform.Rotate(angle.x);
-            this.transform.rotation = Quaternion.Euler(angle);
-        }*/
+            GameObject bullets = Instantiate(Bullet) as GameObject;
+            // 弾丸の位置を調整
+            bullets.transform.position = Muzzle.position;
+            
+            isFirePer = false;
+            StartCoroutine(ReCharge());
+        }
+        
+
+    }
+
+
+
+    //連射間隔調整関数//
+    float time=0f;
+    IEnumerator ReCharge()
+    {
+        while (true)
+        {
+            time += Time.deltaTime;
+            if (time > rechargeTime)
+            {
+                time = 0f;
+                isFirePer = true;
+                break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+
+    //moveX//
+    void moveX(float dirX, bool isRun)
+    {
+        moveDirection.x = dirX;
+        if (isRun)
+        {
+            moveDirection *= runSpeed;      //早く動く
+        }
+        moveDirection *= walkSpeed;     //通常速度
+        controller.Move(moveDirection * Time.deltaTime); //Playerを動かす処理
+    }
+
+
+    //moveZ//
+    void moveZ(float dirY, bool isRun)
+    {
+        moveDirection.z = dirY;
+        if (isRun)
+        {
+            moveDirection *= runSpeed;      //速く動く
+        }
+        moveDirection *= walkSpeed;     //通常速度
+
+        controller.Move(moveDirection * Time.deltaTime); //Playerを動かす処理
+    }
+
+
+    //jump//
+    void jump()
+    {
+        moveDirection.y = 0.0f;
+        moveDirection.y += jumpPower;
+        moveDirection.y += Physics.gravity.y * fallSpeed; //重力計算
+        controller.Move(moveDirection * Time.deltaTime); //Playerを動かす処理
+    }
+
+
+    //angleX//
+    void angleX(float angX)
+    {
+        angle.x = angX;
+
+        cameraDir += new Vector3(-angle.y, angle.x, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(cameraDir);       //カメラ向き
+
+        playerDir += new Vector3(-angle.y, angle.x, 0);
+        this.transform.rotation = Quaternion.Euler(playerDir);      //プレイヤー向き
+    }
+
+
+    //angleY//
+    void angleY(float angY)
+    {
+        angle.y = angY;
+
+        cameraDir += new Vector3(-angle.y, angle.x, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(cameraDir);       //カメラ向き
+
+        playerDir += new Vector3(-angle.y, angle.x, 0);
+        this.transform.rotation = Quaternion.Euler(playerDir);      //プレイヤー向き
+    }
+
+
+    //射撃//
+    void pressRT(bool isPreRT)
+    {
+        if (isPreRT && isFirePer)
+        {
+            GameObject bullets = Instantiate(Bullet) as GameObject;
+            bullets.transform.position = Muzzle.position;       // 弾丸の出現位置を調整
+
+            isFirePer = false;
+            StartCoroutine(ReCharge());
+        }
         
     }
 }
