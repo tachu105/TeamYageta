f313e600728d4acc8e55b68543ea1c4e6b75e9d6
Brayan-SIA <brayan.abe.rasis@gmail.com>
2021-02-19 00:33:54 +0900

    Controllerボタン追加

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -7,51 +7,119 @@ public interface InputInterface
     /// <summary>
     /// Lスティック上下入力
     /// </summary>
-    /// <param name="moveVal">入力値 -1 ~ +1 </param>
-    void moveZ(float moveVal);
+    /// <param name="val">入力値 -1 ~ +1 </param>
+    void LstickVertical(float val);
 
     /// <summary>
     /// Lスティック左右入力
     /// </summary>
-    /// <param name="moveVal">入力値 -1 ~ +1 </param>
-    void moveX(float moveVal);
+    /// <param name="val">入力値 -1 ~ +1 </param>
+    void LstickHorizontal(float val);
 
     /// <summary>
     /// Rスティック左右入力
     /// </summary>
-    /// <param name="moveVal">入力値　-1 ~ +1</param>
-    void lookX(float moveVal);
+    /// <param name="val">入力値　-1 ~ +1</param>
+    void RstickHorizontal(float val);
 
     /// <summary>
     /// Rスティック上下入力　
     /// </summary>
-    /// <param name="moveVal">入力値　-1 ~ +1</param>
-    void lookY(float moveVal);
+    /// <param name="val">入力値　-1 ~ +1</param>
+    void RstickVertical(float val);
 
     /// <summary>
-    /// 射撃(RT)
+    /// Aボタン
     /// </summary>
-    void pressRT();
+    void PressA();
+
+    /// <summary>
+    /// Bボタン
+    /// </summary>
+    void PressB();
+
+    /// <summary>
+    /// Xボタン
+    /// </summary>
+    void PressX();
+
+    /// <summary>
+    /// Yボタン
+    /// </summary>
+    void PressY();
+
+    /// <summary>
+    /// RT入力
+    /// </summary>
+    void PressRT();
+
+    /// <summary>
+    /// LT入力
+    /// </summary>
+    void PressLT();
+
+    /// <summary>
+    /// RB入力
+    /// </summary>
+    void PressRB();
+
+    /// <summary>
+    /// LB入力
+    /// </summary>
+    void PressLB();
+
+    /// <summary>
+    /// Lスティック押し込み
+    /// </summary>
+    void PressL();
+
+    /// <summary>
+    /// Rスティック押し込み
+    /// </summary>
+    void PressR();
+
 }
 
 public class InputController : MonoBehaviour
 {
     public bool isUsePad = false;
-    private Player player;
+    private InputInterface player;
 
-    protected float L_Z;
-    protected float L_X;
-    protected float R_Z;
-    protected float R_X;
+    protected float L_V;
+    protected float L_H;
+    protected float R_V;
+    protected float R_H;
     protected bool RT = false;
+    protected bool LT = false;
+    protected bool RB = false;
+    protected bool LB = false;
+    protected bool A = false;
+    protected bool B = false;
+    protected bool X = false;
+    protected bool Y = false;
+    protected bool L = false;
+    protected bool R = false;
 
-    [SerializeField] private string shootKey = "k";
+    [SerializeField] private string keyUp = "w";
+    [SerializeField] private string keyDown = "s";
+    [SerializeField] private string keyLeft = "a";
+    [SerializeField] private string keyRight = "d";
+    [SerializeField] private string keyA = "z";
+    [SerializeField] private string keyB = "x";
+    [SerializeField] private string keyX = "c";
+    [SerializeField] private string keyY = "v";
+    [SerializeField] private string keyRT = "l";
+    [SerializeField] private string keyLT = "k";
+    [SerializeField] private string keyRB = "o";
+    [SerializeField] private string keyLB = "i";
+    [SerializeField] private string keyL = "left shift";
+    [SerializeField] private string keyR = "right shift";
 
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<Player>();
+        player = GetComponent<InputInterface>();
     }
 
     // Update is called once per frame
@@ -64,38 +132,86 @@ public class InputController : MonoBehaviour
 
     void checkPadInput()
     {
-        L_Z = Input.GetAxis("Vertical");
-        if (L_Z != 0) player.moveZ(L_Z);
-        L_X = Input.GetAxis("Horizontal");
-        if (L_X != 0) player.moveX(L_X);
-        L_Z = Input.GetAxis("Vertical2");
-        if (L_Z != 0) player.moveZ(L_Z);
-        L_X = Input.GetAxis("Horizontal2");
-        if (L_X != 0) player.moveX(L_X);
+        L_V = Input.GetAxis("L Stick Vertical");
+        if (L_V != 0) player.LstickVertical(L_V);
+        L_H = Input.GetAxis("L Stick Horizontal");
+        if (L_H != 0) player.LstickHorizontal(L_H);
+        R_V = Input.GetAxis("R Stick Vertical");
+        if (R_V != 0) player.RstickVertical(R_V);
+        R_H = Input.GetAxis("R Stick Horizontal");
+        if (R_H != 0) player.RstickHorizontal(R_H);
 
-        RT = Input.GetButtonDown("RT");
-        if (RT) player.pressRT();
+        A = Input.GetButtonDown("A button");
+        if (A) player.PressA();
+        B = Input.GetButtonDown("B button");
+        if (B) player.PressB();
+        X = Input.GetButtonDown("X button");
+        if (X) player.PressX();
+        Y = Input.GetButtonDown("Y button");
+        if (Y) player.PressY();
+
+        float trigger = Input.GetAxis("LT RT");
+        if (trigger < 0f)
+        {
+            LT = true;
+            player.PressLT();
+        }
+        else if (trigger > 0f)
+        {
+            RT = true;
+            player.PressRT();
+        }
+        else RT = LT = false;
+
+        RB = Input.GetButtonDown("RB");
+        if (RB) player.PressRB();
+        LB = Input.GetButtonDown("LB");
+        if (LB) player.PressLB();
+
+        R = Input.GetButtonDown("R button");
+        if (R) player.PressR();
+        L = Input.GetButtonDown("L button");
+        if (L) player.PressL();
     }
 
     void checkKeybordInput()
     {
-        if (Input.GetKey("w") && Input.GetKey("s")) L_Z = 0f;
-        else if (Input.GetKey("w")) L_Z = 1f;
-        else if (Input.GetKey("s")) L_Z = -1f;
-        else L_Z = 0f;
-        if (L_Z != 0) player.moveZ(L_Z);
-        if (Input.GetKey("a") && Input.GetKey("d")) L_X = 0f;
-        else if (Input.GetKey("a")) L_X = -1f;
-        else if (Input.GetKey("d")) L_X = 1f;
-        else L_Z = 0f;
-        if (L_X != 0) player.moveX(L_X);
+        if (Input.GetKey(keyUp) && Input.GetKey(keyDown)) L_V = 0f;
+        else if (Input.GetKey(keyUp)) L_V = 1f;
+        else if (Input.GetKey(keyDown)) L_V = -1f;
+        else L_V = 0f;
+        if (L_V != 0) player.LstickVertical(L_V);
+        if (Input.GetKey(keyLeft) && Input.GetKey(keyRight)) L_H = 0f;
+        else if (Input.GetKey(keyLeft)) L_H = -1f;
+        else if (Input.GetKey(keyRight)) L_H = 1f;
+        else L_H = 0f;
+        if (L_H != 0) player.LstickHorizontal(L_H);
 
-        L_Z = Input.GetAxis("Mouse Y");
-        if (L_Z != 0) player.moveZ(R_Z);
-        L_X = Input.GetAxis("Mouse X");
-        if (L_X != 0) player.moveX(R_X);
+        R_V = Input.GetAxis("Mouse X");
+        if (R_V != 0) player.RstickVertical(R_V);
+        R_H = Input.GetAxis("Mouse Y");
+        if (R_H != 0) player.RstickHorizontal(R_H);
 
-        RT = Input.GetKey(KeyCode.KeypadEnter);
-        if (RT) player.pressRT();
+        A = Input.GetKey(keyA);
+        if (A) player.PressA();
+        B = Input.GetKey(keyB);
+        if (B) player.PressB();
+        X = Input.GetKey(keyX);
+        if (X) player.PressX();
+        Y = Input.GetKey(keyY);
+        if (Y) player.PressY();
+
+        RT = Input.GetKey(keyRT);
+        if (RT) player.PressRT();
+        LT = Input.GetKey(keyLT);
+        if (LT) player.PressLT();
+        RB = Input.GetKey(keyRB);
+        if (RB) player.PressRB();
+        LB = Input.GetKey(keyLB);
+        if (LB) player.PressLB();
+        R = Input.GetKey(keyR);
+        if (R) player.PressR();
+        L = Input.GetKey(keyL);
+        if (L) player.PressL();
     }
 }
