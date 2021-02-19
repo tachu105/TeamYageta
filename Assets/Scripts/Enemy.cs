d5c70caa37bea015ace00189167dd3fad10fc390
Brayan-SIA <brayan.abe.rasis@gmail.com>
2021-02-20 03:32:10 +0900

    エネミーの土台スクリプトの実装

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    /// <summary>体力</summary>
+    [SerializeField] protected int hp;    
+    /// <summary>状態</summary>
+    protected EnemyState state;
+    [SerializeField] protected SearchArea searchArea;
+    public int GetHP() { return hp; }
+    public EnemyState GetState() { return state; }
+    
+    /// <summary>
+    /// 行動
+    /// </summary>
+    public abstract void Action();
+
+    /// <summary>
+    /// 被ダメージ処理
+    /// </summary>
+    /// <param name="damage">ダメージ量</param>
+    public abstract void Damage(int damage);
+
+    /// <summary>
+    /// 死亡時の動作
+    /// </summary>
+    public abstract void Dead();
+
+}
+
+public enum EnemyState
+{
+    Wait,
+    Walk,
+    Chase,
+    Attack,
+    Down,
+    Dead
+}
