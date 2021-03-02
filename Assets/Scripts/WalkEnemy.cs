dd5d08662c06606015ee947d771dc130850d3bfe
Brayan-SIA <brayan.abe.rasis@gmail.com>
2021-03-02 20:03:22 +0900

    無限復活解消

--- Assets/Scripts/WalkEnemy.cs
+++ Assets/Scripts/WalkEnemy.cs
@@ -17,9 +17,9 @@ public class WalkEnemy : Enemy
     [SerializeField] private GameObject jumpBarrage;
     private float totalAngle = 360f;
     private const float ATTACK_ANGLE = 30f;
-    private const float BULLET_CHARGE_TIME = 3f;
+    private const float BULLET_CHARGE_TIME = 2f;
     private const float BULLET_SIZE = 0.5f;
-    private const float JUMP_CHARGE_TIME = 3f;
+    private const float JUMP_CHARGE_TIME = 2f;
 
     Animator animator;
     AudioSource ASource;
@@ -44,6 +44,8 @@ public class WalkEnemy : Enemy
     // Update is called once per frame
     void Update()
     {
+        if (hp < 0) return;
+
         if (isSleeping) return;
 
         if (searchArea.IsDetected())
