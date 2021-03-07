26f6c77959f976ffcf17699e1b0ca85730593c0d
YagetaTatatsuki <dragontree105@gmail.com>
2021-03-07 17:06:06 +0900

    UI途中

---
+++ Assets/Scripts/MainMenuUI/SetUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUp : MonoBehaviour
+{
+    GameObject main;
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = GameObject.Find("GameObject");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.pressed)
+        main.transform.position = Vector3.forward * Mathf.Lerp(-365.6289f, 0f, Time.deltaTime);
+    }
+}
