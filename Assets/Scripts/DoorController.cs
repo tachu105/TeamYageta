d6bf3ea621d3632dcdc035f7b216f9b6aa351d7c
Brayan-SIA <brayan.abe.rasis@gmail.com>
2021-03-08 11:45:32 +0900

    ドアの開閉を実装

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] Enemy[] targetEnemies;
+
+    void Update()
+    {
+        if (isAllClear())
+        {
+            GetComponent<Animator>().SetBool("Open", true);
+        }
+    }
+
+    bool isAllClear()
+    {
+        for(int i = 0; i < targetEnemies.Length; i++)
+        {
+            if (targetEnemies[i].GetHP() > 0f) return false;
+        }
+        return true;
+    }
+}
